0be759dd3501c7b5617359abe485c940c4d17542
ulvi_199517 <ulvi199517@gmail.com>
2021-02-09 19:26:01 -0500

    fully functional quote generator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import particlesConfig from './particles-config';
+import {FaQuoteLeft, FaTwitter} from 'react-icons/fa';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      quote: '',
+      author: '',
+      loading: false
+    }
+  }
+tweetQuote = () => {
+  const {quote, author} = this.state;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
+getQuote = () => {
+  fetch("https://favqs.com/api/qotd", {
+	"method": "GET",
+	"headers": {
+		"Content-Type": "application/json",
+		"Authorization": 'Token token="a4cc0569a80b1af6eef203ef9e6be28b"'
+	}
+})
+.then(res => res.json())
+.then(data => this.setState({quote: data.quote.body, author: data.quote.author}))
+.catch(err => {
+	console.error(err);
+});
+
+};
+
+componentDidMount() {
+  this.getQuote();
+};
+
+  
+  
+  
+
+  render(){
+    const {quote, author} = this.state;
+    return(
+      <div className="App">
+        <Particles className='particles' params={particlesConfig} />
+        <div className='quote-container'>
+          <div className='quote-text'>
+            <FaQuoteLeft style={{ paddingRight: '10px', fontSize: '4rem'}}/>
+              <span className='quote'>
+              {quote}
+              </span>
+          </div>
+          <div className='quote-author'>
+            <span>{author}</span>
+          </div>
+          <div className='button-container'>
+            <button className='twitter-button' title='Tweet This!' onClick={this.tweetQuote}>
+              <FaTwitter className='twitter-icon'/>
+            </button>
+            <button className='new-quote' onClick={this.getQuote}>New Quote</button>
+  
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/particles-config.js
@@ -0,0 +1,26 @@
+const particlesConfig = {
+  "particles": {
+    "number": {
+      "value": 99,
+      "density": {
+        "enable": true,
+        value_area: 600
+      }
+    },
+    "color": {
+      "value": "#ded9d9"
+    },
+    "opacity": {
+      "value": 5
+    },
+    "size": {
+      "value": 1,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 50
+      }
+    }
+  }
+}
+export default particlesConfig;
